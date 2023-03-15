11ba2fa937a2ba7044178ebacd0a02fdf35b436e
Umit <umittas2000@gmail.com>
2023-03-15 16:34:49 -0300

    CommonMethods.DataFilePath()
    New method added for file path

--- KDFTest1/Login.cs
+++ KDFTest1/Login.cs
@@ -21,14 +21,21 @@ namespace KDFTest1
 
         public void LoginWithValidCredentials()
         {
-            var lines = loadData.Read(Environment.GetEnvironmentVariable("PATH_DATA_SOURCE"));
+            
+            //var lines = loadData.Read(Environment.GetEnvironmentVariable("PATH_DATA_SOURCE"));
+            
+            //New method added for file path
+            var lines = loadData.Read(CommonMethods.DataFilePath());
+            
             loadData.ExecuteTestCases(lines, driver);
         }
 
 
         public void LoginWithInvalidCredentials()
         {
-            var lines = loadData.Read(Environment.GetEnvironmentVariable("PATH_DATA_SOURCE"));
+            //var lines = loadData.Read(Environment.GetEnvironmentVariable("PATH_DATA_SOURCE"));
+            //New method added for file path
+            var lines = loadData.Read(CommonMethods.DataFilePath());
             loadData.ExecuteTestCases(lines, driver);
         }
     }
